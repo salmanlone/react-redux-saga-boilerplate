862fdb1601eb990a0834e73d1d9a67d15db1f579
Lone <salman.lone@monster.com>
2020-01-23 16:47:20 +0800

    first page setup with all basic settings.

---
+++ app/components/LoadingIndicator/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// import Circle from './Circle';
+// import Wrapper from './Wrapper';
+
+const LoadingIndicator = () => <div></div>;
+
+export default LoadingIndicator;
