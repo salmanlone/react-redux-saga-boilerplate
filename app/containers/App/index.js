059b9534f033266f908067c21ed5b10eda63ef6f
Lone <salman.lone@monster.com>
2020-01-29 13:43:51 +0800

    removing UI to verify architecture

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,32 +8,6 @@ import Footer from "components/Footer";
 
 import GlobalStyle from "../../global-styles";
 
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
-
 export default function App() {
-  return (
-    <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
-      </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        {/* <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} /> */}
-      </Switch>
-      <Footer />
-      <GlobalStyle />
-    </AppWrapper>
-  );
+  return <>Test</>;
 }
