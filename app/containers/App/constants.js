0508b735af8a46fb8bcc15182449bd05dac947a6
Lone <salman.lone@monster.com>
2020-01-30 13:48:23 +0800

    changing namespace

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -9,6 +9,7 @@
  * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
  */
 
-export const LOAD_REPOS = "boilerplate/App/LOAD_REPOS";
-export const LOAD_REPOS_SUCCESS = "boilerplate/App/LOAD_REPOS_SUCCESS";
-export const LOAD_REPOS_ERROR = "boilerplate/App/LOAD_REPOS_ERROR";
+export const LOAD_REPOS = "monster/seeker/salary/App/LOAD_REPOS";
+export const LOAD_REPOS_SUCCESS =
+  "monster/seeker/salary/App/LOAD_REPOS_SUCCESS";
+export const LOAD_REPOS_ERROR = "monster/seeker/salary/App/LOAD_REPOS_ERROR";
