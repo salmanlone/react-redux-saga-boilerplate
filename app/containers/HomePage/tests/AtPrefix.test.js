e4f57ed32e9cc1390b1471ae791e7ba8528ac1fe
Lone <salman.lone@monster.com>
2020-01-30 13:48:44 +0800

    fixed test package migration

--- app/containers/HomePage/tests/AtPrefix.test.js
+++ app/containers/HomePage/tests/AtPrefix.test.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { render } from 'react-testing-library';
+import React from "react";
+import { render } from "@testing-library/react";
 
-import AtPrefix from '../AtPrefix';
+import AtPrefix from "../AtPrefix";
 
-describe('<AtPrefix />', () => {
-  it('should render an <span> tag', () => {
+describe("<AtPrefix />", () => {
+  it("should render an <span> tag", () => {
     const {
-      container: { firstChild },
+      container: { firstChild }
     } = render(<AtPrefix />);
-    expect(firstChild.tagName).toEqual('SPAN');
+    expect(firstChild.tagName).toEqual("SPAN");
   });
 
-  it('should have a class attribute', () => {
+  it("should have a class attribute", () => {
     const {
-      container: { firstChild },
+      container: { firstChild }
     } = render(<AtPrefix />);
-    expect(firstChild.hasAttribute('class')).toBe(true);
+    expect(firstChild.hasAttribute("class")).toBe(true);
   });
 
-  it('should adopt a valid attribute', () => {
-    const id = 'test';
+  it("should adopt a valid attribute", () => {
+    const id = "test";
     const {
-      container: { firstChild },
+      container: { firstChild }
     } = render(<AtPrefix id={id} />);
     expect(firstChild.id).toEqual(id);
   });
 
-  it('should not adopt an invalid attribute', () => {
+  it("should not adopt an invalid attribute", () => {
     const {
-      container: { firstChild },
+      container: { firstChild }
     } = render(<AtPrefix attribute="test" />);
-    expect(firstChild.hasAttribute('attribute')).toBe(false);
+    expect(firstChild.hasAttribute("attribute")).toBe(false);
   });
 });
--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -2,38 +2,38 @@
  * Test the HomePage
  */
 
-import React from 'react';
-import { render } from 'react-testing-library';
-import { IntlProvider } from 'react-intl';
-import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router-dom';
+import React from "react";
+import { render } from "@testing-library/react";
+import { IntlProvider } from "react-intl";
+import { Provider } from "react-redux";
+import { browserHistory } from "react-router-dom";
 
-import { HomePage, mapDispatchToProps } from '../index';
-import { changeUsername } from '../actions';
-import { loadRepos } from '../../App/actions';
-import configureStore from '../../../configureStore';
+import { HomePage, mapDispatchToProps } from "../index";
+import { changeUsername } from "../actions";
+import { loadRepos } from "../../App/actions";
+import configureStore from "../../../configureStore";
 
-describe('<HomePage />', () => {
+describe("<HomePage />", () => {
   let store;
 
   beforeAll(() => {
     store = configureStore({}, browserHistory);
   });
 
-  it('should render and match the snapshot', () => {
+  it("should render and match the snapshot", () => {
     const {
-      container: { firstChild },
+      container: { firstChild }
     } = render(
       <Provider store={store}>
         <IntlProvider locale="en">
           <HomePage loading={false} error={false} repos={[]} />
         </IntlProvider>
-      </Provider>,
+      </Provider>
     );
     expect(firstChild).toMatchSnapshot();
   });
 
-  it('should fetch the repos on mount if a username exists', () => {
+  it("should fetch the repos on mount if a username exists", () => {
     const submitSpy = jest.fn();
     render(
       <Provider store={store}>
@@ -44,24 +44,24 @@ describe('<HomePage />', () => {
             onSubmitForm={submitSpy}
           />
         </IntlProvider>
-      </Provider>,
+      </Provider>
     );
     expect(submitSpy).toHaveBeenCalled();
   });
 
-  it('should not call onSubmitForm if username is an empty string', () => {
+  it("should not call onSubmitForm if username is an empty string", () => {
     const submitSpy = jest.fn();
     render(
       <Provider store={store}>
         <IntlProvider locale="en">
           <HomePage onChangeUsername={() => {}} onSubmitForm={submitSpy} />
         </IntlProvider>
-      </Provider>,
+      </Provider>
     );
     expect(submitSpy).not.toHaveBeenCalled();
   });
 
-  it('should not call onSubmitForm if username is null', () => {
+  it("should not call onSubmitForm if username is null", () => {
     const submitSpy = jest.fn();
     render(
       <Provider store={store}>
@@ -72,43 +72,43 @@ describe('<HomePage />', () => {
             onSubmitForm={submitSpy}
           />
         </IntlProvider>
-      </Provider>,
+      </Provider>
     );
     expect(submitSpy).not.toHaveBeenCalled();
   });
 
-  describe('mapDispatchToProps', () => {
-    describe('onChangeUsername', () => {
-      it('should be injected', () => {
+  describe("mapDispatchToProps", () => {
+    describe("onChangeUsername", () => {
+      it("should be injected", () => {
         const dispatch = jest.fn();
         const result = mapDispatchToProps(dispatch);
         expect(result.onChangeUsername).toBeDefined();
       });
 
-      it('should dispatch changeUsername when called', () => {
+      it("should dispatch changeUsername when called", () => {
         const dispatch = jest.fn();
         const result = mapDispatchToProps(dispatch);
-        const username = 'mxstbr';
+        const username = "mxstbr";
         result.onChangeUsername({ target: { value: username } });
         expect(dispatch).toHaveBeenCalledWith(changeUsername(username));
       });
     });
 
-    describe('onSubmitForm', () => {
-      it('should be injected', () => {
+    describe("onSubmitForm", () => {
+      it("should be injected", () => {
         const dispatch = jest.fn();
         const result = mapDispatchToProps(dispatch);
         expect(result.onSubmitForm).toBeDefined();
       });
 
-      it('should dispatch loadRepos when called', () => {
+      it("should dispatch loadRepos when called", () => {
         const dispatch = jest.fn();
         const result = mapDispatchToProps(dispatch);
         result.onSubmitForm();
         expect(dispatch).toHaveBeenCalledWith(loadRepos());
       });
 
-      it('should preventDefault if called with event', () => {
+      it("should preventDefault if called with event", () => {
         const preventDefault = jest.fn();
         const result = mapDispatchToProps(() => {});
         const evt = { preventDefault };
